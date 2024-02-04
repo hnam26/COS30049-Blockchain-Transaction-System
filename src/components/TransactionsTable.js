32b74d1cdafc4e50048f2925177cb3f6996947a9
Quang Thien Le <thienlequang62ld@gmail.com>
2024-02-04 23:50:11 +0700

    adad

--- src/components/TransactionsTable.js
+++ src/components/TransactionsTable.js
@@ -2,35 +2,6 @@ import React from "react";
 
 const TransactionsTable = ({ transactions }) => {
   return (
-    // <table className="transactions-table">
-    //   <thead>
-    //     <tr>
-    //       <th>ID</th>
-    //       <th>Sender</th>
-    //       <th>Receiver</th>
-    //       <th>Type</th>
-    //       <th>Amount</th>
-    //       <th>Fee</th>
-    //       <th>Status</th>
-    //       <th>Date</th>
-    //     </tr>
-    //   </thead>
-    //   <tbody>
-    //     {transactions.map((transaction, index) => (
-    //       <tr key={index}>
-    //         <td>{transaction.transactionId}</td>
-    //         <td>{transaction.sender}</td>
-    //         <td>{transaction.receiver}</td>
-    //         <td>{transaction.type}</td>
-    //         <td>{transaction.amount} </td>
-    //         <td>{transaction.fee}</td>
-    //         <td>{transaction.status}</td>
-    //         <td>{transaction.date}</td>
-    //       </tr>
-    //     ))}
-    //   </tbody>
-    // </table>
-
     <div className="table-responsive">
       <table className="transactions-table">
         <thead>
