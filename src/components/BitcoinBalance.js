5015e1f3450d95aedc904dd39de86abf149dc71d
hnam26 <hoainamkin03@gmail.com>
2024-02-04 11:02:04 +0700

    update search

--- src/components/BitcoinBalance.js
+++ src/components/BitcoinBalance.js
@@ -9,9 +9,6 @@ const BitcoinBalance = () => {
           12.2081401 <span>$ 12,345.67</span>
         </p>
       </div>
-      <div className="search-wallet">
-        <input type="text" placeholder="Search wallet address..." />
-      </div>
     </section>
   );
 };
