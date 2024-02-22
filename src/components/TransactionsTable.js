1340304c12cb3230652d068ea2bbe64b700b06d1
Quang Thien Le <thienlequang62ld@gmail.com>
2024-02-22 10:21:26 +0700

    Change code in transaction table

--- src/components/TransactionsTable.js
+++ src/components/TransactionsTable.js
@@ -3,41 +3,36 @@ import React from "react";
 const TransactionsTable = ({ transactions }) => {
   return (
     <>
-    <h2>Transactions History</h2>
-    {/* Display the transactions in a table */}
-    <table className="transactions-table">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Sender</th>
-          <th>Receiver</th>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Fee</th>
-          <th>Status</th>
-          <th>Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        {transactions.map((transaction, index) => (
-          <tr key={index}>
-            <td>{transaction.transactionId}</td>
-            <td>{transaction.sender}</td>
-            <td>{transaction.receiver}</td>
-            <td>{transaction.type}</td>
-            <td>
-              {/* Display both expense and income in the "Amount" field */}
-              Expense: {transaction.expense}
-              <br />
-              Income: {transaction.income}
-            </td>
-            <td>{transaction.fee}</td>
-            <td>{transaction.status}</td>
-            <td>{transaction.date}</td>
+      <h2>Transactions History</h2>
+      {/* Display the transactions in a table */}
+      <table className="transactions-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Sender</th>
+            <th>Receiver</th>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Fee</th>
+            <th>Status</th>
+            <th>Date</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {transactions.map((transaction, index) => (
+            <tr key={index}>
+              <td>{transaction.transactionId}</td>
+              <td>{transaction.sender}</td>
+              <td>{transaction.receiver}</td>
+              <td>{transaction.type}</td>
+              <td>{transaction.amount}</td>
+              <td>{transaction.fee}</td>
+              <td>{transaction.status}</td>
+              <td>{transaction.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
